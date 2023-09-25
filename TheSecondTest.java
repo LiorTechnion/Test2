144c63a0d6999040897cd15b406a1df56b01a7d4
Lior <128314109+LiorTechnion@users.noreply.github.com>
2023-09-25 03:41:05 +0300

    Update TheSecondTest.java
    Creation of a class that represents a student.

--- TheSecondTest.java
+++ TheSecondTest.java
@@ -1,5 +1,41 @@
 public class TheSecondTest {
     public static void main(String[] args) {
-        final short EXAMPLE = 110;
+        Student student = new Student()
     }
 }
+
+// An Enum thst represents s faculty.
+public Enum Faculty {
+    String CS;
+    String DDS;
+    String EE;
+}
+
+// A class that represents a student.
+public class Student {
+    private final String name;
+    private float age;
+    private final String Mail;
+    private Faculty faculty;
+    private String address;
+
+    public Student(String name, float age, String Mail, Faculty faculty, String address) {
+        this.name = name;
+        this.
+        this.
+        this.
+        this.
+    }
+
+    public Student() {}
+
+}
+    /*Do this for each field*/
+    public /*retun value*/ get/*field*/() {
+        return this./*field*/
+    }
+
+    /*Do this for each field*/
+    public void set/*field*/(/*field value*/ /*field*/) {
+        this./*field*/ = /*field*/
+    }
